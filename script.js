21eadf49fca4720b80852dfb557da83957d8ec30
Nathan <nathanuyng898@gmail.com>
2026-06-04 06:41:24 +0800

    added startQuiz func

--- script.js
+++ script.js
@@ -74,7 +74,16 @@ maxScoreSpan.textContent = quizQuestions.length;
 startButton.addEventListener("click", startQuiz);
 restartButton.addEventListener("click", restartQuiz);
 
-function startQuiz() {
+function startQuiz() {\
+  // reset vars
+  currentQuestionIndex = 0;
+  score = 0;
+  scoreSpan.textContent = 0;
+
+  startScreen.classList.remove("active");
+  quizScreen.classList.add("active");
+
+  showQuestion();
 }
 
 function showQuestion() {
